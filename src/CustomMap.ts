583da56fe1f02071392dbc2dc2c92233b83ec9ef
David Yoon <davidyoon85@gmail.com>
2021-02-23 21:12:03 -0500

    add map markers

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,11 @@
+interface Mappable {
+    location: {
+        lat: number,
+        lng: number
+    },
+    markerContent(): string;
+}
+
 export class CustomMap {
     private googleMap: google.maps.Map;
 
@@ -10,4 +18,24 @@ export class CustomMap {
             }
         });
     }
+
+    addMarker(mappable: Mappable): void {
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        })
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            })
+
+            infoWindow.open(this.googleMap, marker);
+
+        })
+    }
+
 }
